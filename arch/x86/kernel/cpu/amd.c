bde81403fe3494f02b8bf823b89736a33b70ae61
Maciej S. Szmigiero <maciej.szmigiero@oracle.com>
2024-01-25 19:06:54 +0100

    x86/CPU/AMD: Fix disabling XSAVES on AMD family 0x17 due to erratum
    The stable kernel version backport of the patch disabling XSAVES on AMD
    Zen family 0x17 applied this change to the wrong function (init_amd_k6()),
    one which isn't called for Zen CPUs.
    
    Move the erratum to the init_amd_zn() function instead.
    
    Add an explicit family 0x17 check to the erratum so nothing will break if
    someone naively makes this kernel version call init_amd_zn() also for
    family 0x19 in the future (as the current upstream code does).
    
    Fixes: f028a7db9824 ("x86/CPU/AMD: Disable XSAVES on AMD family 0x17")
    Signed-off-by: Maciej S. Szmigiero <maciej.szmigiero@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 771df0145297a1a9f1e7f799da43f8b0f8850e7c)
    Signed-off-by: Vegard Nossum <vegard.nossum@oracle.com>

--- arch/x86/kernel/cpu/amd.c
+++ arch/x86/kernel/cpu/amd.c
@@ -870,6 +870,17 @@ static void init_amd_zn(struct cpuinfo_x86 *c)
 		if (c->x86 == 0x19 && !cpu_has(c, X86_FEATURE_BTC_NO))
 			set_cpu_cap(c, X86_FEATURE_BTC_NO);
 	}
+
+	/*
+	 * Work around Erratum 1386.  The XSAVES instruction malfunctions in
+	 * certain circumstances on Zen1/2 uarch, and not all parts have had
+	 * updated microcode at the time of writing (March 2023).
+	 *
+	 * Affected parts all have no supervisor XSAVE states, meaning that
+	 * the XSAVEC instruction (which works fine) is equivalent.
+	 */
+	if (c->x86 == 0x17)
+		clear_cpu_cap(c, X86_FEATURE_XSAVES);
 }
 
 static void init_amd(struct cpuinfo_x86 *c)
