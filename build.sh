efb8b0a3d46903868c70f3c324eb9b7675e9a503
noticesax <anatayyasongotelu@gmail.com>
2024-10-05 12:23:34 +0000

    build.sh: switch anykernel branch

--- build.sh
+++ build.sh
@@ -88,7 +88,7 @@ fi
 if [ ! -d "${PWD}/anykernel" ]; then
     echo -e "${RED}error:${NOCOLOR} /anykernel not found!"
     echo "Cloning AnyKernel3..."
-    git clone -b Fearless https://github.com/noticesax/AnyKernel3 anykernel
+    git clone -b Selene https://github.com/noticesax/AnyKernel3 anykernel
     if [ $? -ne 0 ]; then
         echo -e "${RED}error:${NOCOLOR} Failed to clone!"
         exit 1
